b317a039d42c944f3ac1dca5406fbd81b21175ae
StevenGualpa <stevengualpa@gmail.com>
2022-06-19 14:34:00 -0500

    Creacion
    Creacion

---
+++ app/src/main/java/com/geek/a03_kotlin_volley_cardview/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.geek.a03_kotlin_volley_cardview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
